c8959dd3aac8a47c9790fd00db87cb8706e6ab36
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-15 23:28:12 +0600

    validator added

--- model/User.js
+++ model/User.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
+const { isEmail, isStrongPassword } = require("validator");
 
 const UserSchema = mongoose.Schema({
   email: {
     type: String,
-    requried: true,
+    requried: [true, `Please enter an email`],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
-    requried: true,
+    requried: [true, `Please enter a password`],
     unique: true,
-    minLength: 8,
+    minlength: [8, "Please enter minimum 8 characters"],
+    validate: [isStrongPassword, "Please enter strong password"],
   },
 });
 
